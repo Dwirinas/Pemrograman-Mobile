7b5f000866c8aa86d5f2ee5b5dab5f56681cb53d
Dwirinas <Risinasa9b@gmail.com>
2019-10-09 08:21:01 +0700

    Modul 4

--- MyViewAndViews_Modul3/app/src/main/java/com/rina/myviewandviews/MainActivity.java
+++ MyViewAndViews_Modul3/app/src/main/java/com/rina/myviewandviews/MainActivity.java
@@ -2,9 +2,13 @@ package com.rina.myviewandviews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button donasi_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
         if(getSupportActionBar() !=null){
             getSupportActionBar().setTitle("Donasi Pesawat Terbang");
         }
+
+        donasi_btn = findViewById(R.id.donasi_btn);
+
+        donasi_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DonasiActivity.class));
+            }
+        });
     }
 }
