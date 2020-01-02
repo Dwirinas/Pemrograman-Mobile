4cae306ae7f19557edbfa869281196a0c97020d0
Dwirinas <Risinasa9b@gmail.com>
2020-01-02 11:53:53 +0700

    Modul 14

---
+++ MyClassIK2B/app/src/main/java/com/rina/myclass_ik_2b/DataHelper.java
@@ -0,0 +1,6 @@
+package com.rina.myclass_ik_2b;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataHelper extends SQLiteOpenHelper {
+}
---
+++ MyClassIK2B/app/src/main/java/com/rina/myclass_ik_2b/LihatData.java
@@ -0,0 +1,14 @@
+package com.rina.myclass_ik_2b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LihatData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_data);
+    }
+}
