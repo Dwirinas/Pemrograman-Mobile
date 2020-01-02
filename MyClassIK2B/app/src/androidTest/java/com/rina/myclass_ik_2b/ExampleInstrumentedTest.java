dbfc1b0423e5631aa9fe8647630aee6422e5afca
Dwirinas <Risinasa9b@gmail.com>
2020-01-02 11:57:49 +0700

    Modul 15

--- MyClassIK2B/app/src/androidTest/java/com/rina/myclass_ik_2b/ExampleInstrumentedTest.java
+++ MyClassIK2B/app/src/androidTest/java/com/rina/myclass_ik_2b/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.rina.myclass_ik_2b;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
